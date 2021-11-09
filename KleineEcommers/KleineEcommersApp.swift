b7450865e236d6885fa1d7a72997246a43a6a3f1
Nabil_saadi <nabil.saadi@codersdc.com>
2021-11-09 09:24:17 +0200

    Initial Commit

---
+++ KleineEcommers/KleineEcommersApp.swift
@@ -0,0 +1,17 @@
+//
+//  KleineEcommersApp.swift
+//  KleineEcommers
+//
+//  Created by Yousef on 11/9/21.
+//
+
+import SwiftUI
+
+@main
+struct KleineEcommersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
